bd3b00a0ae5713b2b0eddb7b9066c1cad58abff0
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2018-08-23 22:51:30 -0300

    Atividade X - Resolução

--- AtividadeX_ROGERIO/eleicoes.py
+++ AtividadeX_ROGERIO/eleicoes.py
@@ -5,18 +5,41 @@ import os
 
 def main():
 
-    # listas vazias
+    # Variaveis gerais
     coligacoes = []
     vereadores = []
+    vereadores_eleitos = []
+    total_votos = 0
+    quociente_eleitoral = 0
+    qtd_vagas = 29
+    qtd_vagas_sobra = 0
 
     # preencher listas com dados importados: lista de dicionarios
     inicializar_dados(coligacoes, vereadores)
+
+    # Computacoes iniciais
+    total_votos = total_votos_validos(vereadores)
+    quociente_eleitoral = total_votos // qtd_vagas
+
+    # Distribuir Vagas
+    qtd_vagas_sobra = calcular_qtd_vagas_por_quociente_eleitoral(coligacoes, qtd_vagas, quociente_eleitoral)
+    calcular_qtd_vagas_por_media(coligacoes, qtd_vagas_sobra)
+
+    # Marcar eleitos e filtrá-los
+    marca_vereadores_eleitos(coligacoes, vereadores)
+    vereadores_eleitos = filtrar_conjunto(vereadores, 'eleito', True)
+
+    # Ordenar eleitos e coligacoes por Votos
+    bubble_sort(vereadores_eleitos, chave=lambda x: x['total_votos'], inverso=True)
+    bubble_sort(coligacoes, chave=lambda x:x['qtd_vagas'], inverso=True)
 
     # opcoes FAKE, demonstracao apenas
     menu = '***** ELEIÇÕES TERESINA/2012 *****\n' \
            '1 - Total de Coligacoes\n' \
            '2 - Listar Coligacoes\n' \
            '3 - Total Votos Válidos\n' \
+           '4 - Quociente Eleitoral\n' \
+           '6 - Vereadores eleitos\n' \
            '0 - Sair\n'
 
     # Loop do Menu de opcões
@@ -32,7 +55,11 @@ def main():
         elif opcao == 2:
             lista_todos(coligacoes)
         elif opcao == 3:
-            total_votos_validos(vereadores)
+            print('Total Votos = {}'.format(total_votos))
+        elif opcao == 4:
+            print('Quociente Eleitoral = {:.2f}'.format(quociente_eleitoral))
+        elif opcao == 6:
+            lista_todos(vereadores_eleitos)
         else:
             print('Opcao Invalida!')
 
@@ -51,15 +78,6 @@ def inicializar_dados(coligacoes, vereadores):
     print('Carregados {} coligacoes e {} vereadores'.format(len(dados_coligacoes), len(dados_vereadores)))
 
     # Organizar dados de acordo com o solicitado
-    # Coligações
-    for item in dados_coligacoes:
-        coligacao = {}
-        coligacao['coligacao'] = item
-        coligacao['total_votos'] = 0
-        coligacao['qtd_vagas'] = 0
-        coligacao['votos_sobra_total'] = 0
-        coligacoes.append(coligacao)
-
     # Vereadores
     for item in dados_vereadores:
         vereador = {}
@@ -68,7 +86,18 @@ def inicializar_dados(coligacoes, vereadores):
         vereador['partido'] = item[2]
         vereador['coligacao'] = item[3]
         vereador['total_votos'] = int(item[4])
+        vereador['eleito'] = False
         vereadores.append(vereador)
+
+    # Coligações
+    for item in dados_coligacoes:
+        coligacao = {}
+        coligacao['coligacao'] = item
+        vereadores_coligacao = filtrar_conjunto(vereadores, 'coligacao', coligacao['coligacao'])
+        coligacao['total_votos'] = reduzir_conjunto(vereadores_coligacao, 'total_votos')
+        coligacao['qtd_vagas'] = 0
+        coligacao['votos_sobra_total'] = 0
+        coligacoes.append(coligacao)
 
 
     # Limpar a tela
--- AtividadeX_ROGERIO/funcionalidades.py
+++ AtividadeX_ROGERIO/funcionalidades.py
@@ -1,10 +1,3 @@
-"""
-Escreva suas funções aqui:
- - Tanto funcoes para funcionalidades do Menu;
- - Quanto funções auxiliares a estas.
-"""
-
-
 # ##### FUNCIONALIDADES MENU #####
 
 def total(dados):
@@ -18,7 +11,40 @@ def lista_todos(dados):
 
 def total_votos_validos(vereadores):
     total = reduzir_conjunto(vereadores, 'total_votos', 'soma')
-    print('Votos válidos = {}'.format(total))
+    return total
+
+
+def atualizar_votos_sobra_total(coligacoes):
+    for coligacao in coligacoes:
+        coligacao['votos_sobra_total'] = coligacao['total_votos'] // (coligacao['qtd_vagas'] + 1)
+
+
+def calcular_qtd_vagas_por_quociente_eleitoral(coligacoes, qtd_vagas, quociente_eleitoral):
+    vagas_distribuidas = 0
+    for coligacao in coligacoes:
+        coligacao['qtd_vagas'] = int(coligacao['total_votos'] // quociente_eleitoral)
+        coligacao['votos_sobra_total'] = coligacao['total_votos'] // (coligacao['qtd_vagas'] + 1)
+        vagas_distribuidas += coligacao['qtd_vagas']
+
+    return qtd_vagas - vagas_distribuidas
+
+
+def calcular_qtd_vagas_por_media(coligacoes, qtd_vagas_sobra):
+    while qtd_vagas_sobra > 0:
+        bubble_sort(coligacoes, chave=lambda x:x['votos_sobra_total'], inverso=True)
+        coligacoes[0]['qtd_vagas'] += 1
+        atualizar_votos_sobra_total(coligacoes)
+        qtd_vagas_sobra -= 1
+
+
+def marca_vereadores_eleitos(coligacoes, vereadores):
+
+    for coligacao in coligacoes:
+        vereadores_coligacao = filtrar_conjunto(vereadores, 'coligacao', coligacao['coligacao'])
+        bubble_sort(vereadores_coligacao, chave=lambda x:x['total_votos'], inverso=True)
+        vereadores_coligacao_eleitos = vereadores_coligacao[:coligacao['qtd_vagas']]
+        for vereador in vereadores_coligacao_eleitos:
+            vereador['eleito'] = True
 
 
 # ### FUNCOES AUXILIARES ###
@@ -30,15 +56,44 @@ def cabecalho(titulo):
 
 
 def reduzir_conjunto(lista, atributo, operacao='soma'):
-    """
-    :param lista: lista de dicionários
-    :param atributo: chave usada para reduce
-    :param operacao: qual operacao utilizada no reduce
-    :return:
-    """
-    acumulado = 0
 
+    acumulado = 0
     for item in lista:
         if operacao == 'soma':
             acumulado += item[atributo]
     return acumulado
+
+
+def filtrar_conjunto(lista, atributo, valor_atributo):
+
+    selecionados = []
+    for item in lista:
+        if item[atributo] == valor_atributo:
+            selecionados.append(item)
+    return selecionados
+
+
+def bubble_sort(dados, chave=None, inverso=False):
+    trocou = True
+    fim = len(dados) - 1
+
+    while trocou:
+        trocou = False
+        for i in range(fim):
+            item_esquerda = dados[i]
+            item_direita = dados[i+1]
+
+            esquerda_maior = item_esquerda > item_direita if chave is None else chave(item_esquerda) > chave(item_direita)
+            esquerda_menor = item_esquerda < item_direita if chave is None else chave(item_esquerda) < chave(item_direita)
+
+            if (not inverso and esquerda_maior) or (inverso and esquerda_menor):
+                trocar(dados, i, item_direita, item_esquerda)
+                trocou = True
+
+        fim -= 1
+
+
+def trocar(dados, i, item_direita, item_esquerda):
+    aux = item_esquerda
+    dados[i] = item_direita
+    dados[i + 1] = aux
